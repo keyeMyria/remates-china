333aa22bc87625405550615dfbb98946a66ce481
Simon Godoy <theonlysimon@gmail.com>
2018-03-06 17:09:44 -0500

    minnor changes in activity

--- sample/src/main/java/com/shopify/sample/view/main/RegisterActivity.java
+++ sample/src/main/java/com/shopify/sample/view/main/RegisterActivity.java
@@ -1,4 +1,13 @@
 package com.shopify.sample.view.main;
 
-public class RegisterActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.shopify.sample.R;
+
+public class RegisterActivity extends AppCompatActivity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_register);
+	}
 }
